1176479fb4754dbd9288248904e7b008231d517e
audumbar <audumbar100bansode@gmail.com>
2019-01-22 12:23:58 +0530

    Sop add

--- DemoProject/src/com/Test.java
+++ DemoProject/src/com/Test.java
@@ -4,7 +4,6 @@ import org.omg.CORBA.OBJECT_NOT_EXIST;
 
 public class Test {
  public static void main(String[] args) {
-	Object o=new Object();
-	Object ok=new Object();
+	System.out.println("hello i am Audumbar");
 }
 }
